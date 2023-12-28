c1499e115cb10b7ea598323e56d0b7d7d795fc19
ckerce <ckerce@gmail.com>
2023-12-28 09:46:39 -0500

    added pseudocode sketch and reference

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ In their paper [Simplifying Transformer Blocks](https://arxiv.org/abs/2311.01906
 
 ![SAS-p](assets/SAS-P.png)
 
+Shaped Attention is developed and detailed by the same group, Noci, et. all in https://arxiv.org/abs/2306.17759
+
 # nanoGPT
 
 nanoGPT (https://github.com/karpathy/nanoGPT) is a simple and fast repository for training/finetuning medium-sized GPTs. It is a rewrite of [minGPT](https://github.com/karpathy/minGPT) that prioritizes teeth over education. Still under active development, but currently the file `train.py` reproduces GPT-2 (124M) on OpenWebText, running on a single 8XA100 40GB node in about 4 days of training. The code itself is plain and readable: `train.py` is a ~300-line boilerplate training loop and `model.py` a ~300-line GPT model definition, which can optionally load the GPT-2 weights from OpenAI. That's it.
--- model.py
+++ model.py
@@ -34,6 +34,39 @@ class LayerNorm(nn.Module):
 ###############################################################################
 
 class CausalShapedAttention(nn.Module):
+    '''
+    nano-GPT style code to implement causal shaped attention and apply it to an input.
+
+    Pseudocode with bad dimensionality:
+    
+    self.W_Q = nn.Linear(self.head_dim, self.head_dim, bias=False)
+    nn.init.zeros_(self.W_Q.weight)
+    self.W_K = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        
+    def forward(self, X):
+        seq_len = X.size(1)  # Assuming X is of shape [batch, seq_len, token_embd_dim]
+        M_tmp = self.M[:seq_len, :seq_len]
+        C_tmp = self.C[:seq_len, :seq_len]
+        # Split X into self.heads chunks along the token_embd_dim dimension
+        X_chunks = torch.chunk(X, self.heads, dim=2)
+
+        # Process each chunk and store the results
+        processed_chunks = []
+        for X_h in X_chunks:
+            Q_h = self.W_Q(X_h)
+            K_h = self.W_K(X_h)
+            QKT = torch.bmm(Q_h, K_h.transpose(2,1)) / self.scale
+            A_h = nn.functional.softmax( QKT, dim=-1) + M_tmp
+            Id = torch.eye( X_h.size(1) )
+            modified_X_h = self.alpha * X_h + (self.beta * A_h - self.gamma * C_tmp) * X_h
+            processed_chunks.append(modified_X_h)
+
+        # Concatenate the processed chunks back together
+        X_out = torch.cat(processed_chunks, dim=2)
+        return X_out
+    
+
+    '''
 
     def __init__(self, config):
         super().__init__()
@@ -95,6 +128,9 @@ class CausalShapedAttention(nn.Module):
         att = self.attn_dropout(att)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        
+        # NOTE:  There is no skip connection, so this is the output of the 
+        # Shaped Attention applied to the input.
 
         return y
 
